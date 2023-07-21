677e4d95e3f521185a761ab6399e06aedfa30dfe
Vinicius Vequeti <viniciusvequeti@gmail.com>
2023-07-21 09:55:26 -0300

    User update with PUT

--- src/main/java/com/viniciusv/workshopmongo/services/UserService.java
+++ src/main/java/com/viniciusv/workshopmongo/services/UserService.java
@@ -35,6 +35,18 @@ public class UserService {
 		userRepository.deleteById(id);
 	}
 	
+	public User update(User user) {
+		Optional<User> optionalUser = userRepository.findById(user.getId());
+		User newUser = optionalUser.orElseThrow(() -> new ObjectNotFoundException("Id não encontrado"));
+		updateData(newUser, user);
+		return userRepository.save(newUser);
+	}
+	
+	private void updateData(User newUser, User user) {
+		newUser.setName(user.getName());
+		newUser.setEmail(user.getEmail());
+	}
+	
 	public User fromDTO(UserDTO userDTO) {
 		return new User(userDTO.getId(), userDTO.getName(), userDTO.getEmail());
 	}
