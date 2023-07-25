80de516498761070d9278cfb72442316c955cbf8
Vinicius Vequeti <viniciusvequeti@gmail.com>
2023-07-25 11:32:56 -0300

    Posts endpoint and retrieve by posts id

---
+++ src/main/java/com/viniciusv/workshopmongo/resources/PostResource.java
@@ -0,0 +1,33 @@
+package com.viniciusv.workshopmongo.resources;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.viniciusv.workshopmongo.entities.Post;
+import com.viniciusv.workshopmongo.services.PostService;
+
+@RestController
+@RequestMapping(value = "/posts")
+public class PostResource {
+
+	@Autowired
+	private PostService postService;
+	
+	@GetMapping
+	public ResponseEntity<List<Post>> findAll(){
+		List<Post> result = postService.findAll();
+		return ResponseEntity.ok().body(result);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Post> findById(@PathVariable String id){
+		Post post = postService.findById(id);
+		return ResponseEntity.ok().body(post);
+	}
+}
---
+++ src/main/java/com/viniciusv/workshopmongo/services/PostService.java
@@ -0,0 +1,27 @@
+package com.viniciusv.workshopmongo.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.viniciusv.workshopmongo.entities.Post;
+import com.viniciusv.workshopmongo.repositories.PostRepository;
+import com.viniciusv.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository postRepository;
+	
+	public List<Post> findAll(){
+		return postRepository.findAll();
+	}
+	
+	public Post findById(String id) {
+		Optional<Post> user = postRepository.findById(id);
+		return user.orElseThrow(() -> new ObjectNotFoundException("Id não encontrado"));
+	}
+}
