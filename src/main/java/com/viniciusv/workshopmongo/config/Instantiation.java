8e2412e38fcb4a0105bc5ff61384516cd7c57767
Vinicius Vequeti <viniciusvequeti@gmail.com>
2023-07-25 10:34:40 -0300

    AuthorDTO

--- src/main/java/com/viniciusv/workshopmongo/config/Instantiation.java
+++ src/main/java/com/viniciusv/workshopmongo/config/Instantiation.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.viniciusv.workshopmongo.dto.AuthorDTO;
 import com.viniciusv.workshopmongo.entities.Post;
 import com.viniciusv.workshopmongo.entities.User;
 import com.viniciusv.workshopmongo.repositories.PostRepository;
@@ -34,11 +35,12 @@ public class Instantiation implements CommandLineRunner {
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
-
-		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", maria);
-		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", maria);
-
+		
 		userReposiroty.saveAll(Arrays.asList(maria, alex, bob));
+
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+
 		postReposiroty.saveAll(Arrays.asList(post1, post2));
 	}
 
